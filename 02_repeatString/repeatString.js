993ef90e951bbff383844d14fb1bd6e4d4c6aa79
reda. <redadahmani34@gmail.com>
2025-01-22 17:34:24 +0100

    02_repeatString exe

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  if (word < 0) {
+    return "ERROR";
+  }
+  string = "";
+  for (i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
