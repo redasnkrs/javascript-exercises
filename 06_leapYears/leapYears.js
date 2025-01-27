dc60ae840292b1c8ac54d16a93de34416a3abdd0
reda. <redadahmani34@gmail.com>
2025-01-27 07:17:07 +0100

    make all the exercice

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function (isLeap) {
+  isLeap = false;
+  if (isLeap % 4 === 0) {
+    return true;
+  } else if (isLeap % 4 === 0 && isLeap % 100 != 0) {
+    return true;
+  }
 };
 
 // Do not edit below this line
