600896bdb4234185d7a3279c669fa962bf7c6737
reda. <redadahmani34@gmail.com>
2025-01-27 07:12:14 +0100

    tempConversion exe

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  convertC = ((tempF - 32) * 5) / 9;
+  return convertC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  convertF = (9 / 5) * tempC + 32;
+  return convertF;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
