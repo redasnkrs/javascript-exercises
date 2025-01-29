b9d0a0fd306a12bc8433bcfb97cab3591ce17ca1
reda. <redadahmani34@gmail.com>
2025-01-29 07:54:23 +0100

    did the calculatooor

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,15 +18,32 @@ const sum = function (arr) {
   return mySum;
 };
 
-const multiply = function (num1, num2) {
-  return num1 * num2;
+const multiply = function (arr) {
+  let sum = 1;
+  if (arr.length < 0) {
+    return 0;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    sum *= arr[i];
+  }
+  return sum;
 };
 
 const power = function (num1, num2) {
   return num1 ** num2;
 };
 
-const factorial = function () {};
+const factorial = function (num1) {
+  let forsum = 1;
+  if (num1 === 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= num1; i++) {
+      forsum *= i;
+    }
+    return forsum;
+  }
+};
 
 // Do not edit below this line
 module.exports = {
